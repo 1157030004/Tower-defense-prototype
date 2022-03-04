6df1e6f545434fd087d631e27e9f17d053fd6de2
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-03-04 23:11:42 +0700

    set color labeler based on grid manager

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -10,16 +10,18 @@ public class CoordinateLabeler : MonoBehaviour
 
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCooridnates();
     }
 
@@ -45,14 +47,29 @@ public class CoordinateLabeler : MonoBehaviour
     }
 
 
-    void SetLabelColor(){
-        if(waypoint.IsPlaceable)
+    void SetLabelColor()
+    {
+        if(gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if(node == null) { return; }
+
+        if(!node.isWalkable)
         {
-            label.color = defaultColor;
+            label.color = blockedColor;
+        }
+        else if(node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if(node.isExplored)
+        {
+            label.color = exploredColor;
         }
         else
         {
-            label.color = blockedColor;
+            label.color = defaultColor;
         }
     }
 
