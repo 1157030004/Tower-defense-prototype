dde51c877f1fee36ecaa62e6894631ef6a2c0b40
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-03-05 10:21:36 +0700

    refind new path when blocked by towers

--- Assets/PathFinding/Pathfinder.cs
+++ Assets/PathFinding/Pathfinder.cs
@@ -32,8 +32,16 @@ public class Pathfinder : MonoBehaviour
         startNode = gridManager.Grid[startCoordinates];
         destinationNode = gridManager.Grid[destinationCoordinates];
 
+        GetNewPath();
+    }
+
+    public List<Node> GetNewPath()
+    {
+        gridManager.ResetNodes();
         BreadthFirstSearch();
         BuildPath();
+
+        return BuildPath();
     }
 
     void ExploreNeighbours()
@@ -61,6 +69,10 @@ public class Pathfinder : MonoBehaviour
 
     void BreadthFirstSearch()
     {
+
+        frontier.Clear();
+        reached.Clear();
+
         bool isRunning = true;
 
         frontier.Enqueue(startNode);
@@ -95,5 +107,25 @@ public class Pathfinder : MonoBehaviour
 
         path.Reverse();
         return path;
+    }
+
+    public bool WillBlockPath(Vector2Int coordinates)
+    {
+        if(grid.ContainsKey(coordinates))
+        {
+            bool previousState = grid[coordinates].isWalkable;
+            grid[coordinates].isWalkable = false;
+            List<Node> newPath = GetNewPath();
+            grid[coordinates].isWalkable = previousState;
+
+            if(newPath.Count <= 1)
+            {
+                GetNewPath();
+                return true;
+            }
+
+        }
+
+         return false;
     }
 }
