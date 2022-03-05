5595552de3ce4ec21538b131ad1f5fc6cbdf0e51
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-03-05 11:02:42 +0700

    reroute enemy movement when a tower is placed

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -14,7 +14,7 @@ public class EnemyMover : MonoBehaviour
 
     void OnEnable()
     {
-        FindPath();
+        RecalculatePath();
         ReturnToStart();
         StartCoroutine(FollowPath());
     }
@@ -26,7 +26,7 @@ public class EnemyMover : MonoBehaviour
         pathfinder = FindObjectOfType<Pathfinder>();
     }
 
-    void FindPath(){
+    void RecalculatePath(){
         path.Clear();
         path = pathfinder.GetNewPath();
     }
